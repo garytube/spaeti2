86b48e5c4e2cd359af4717da69ef8a5b9782ca9b
Gerald Scholz <gerald.scholz@walldecaux.de>
2023-08-02 18:15:56 +0200

    stuff

--- app/src/lib/components/ShoppingCart/ShoppingCart.svelte
+++ app/src/lib/components/ShoppingCart/ShoppingCart.svelte
@@ -1,44 +1,43 @@
 <script>
-  import CartPanel from "./CartPanel.svelte";
-  import { cartPanelOpen, product } from "$lib/store/productStore";
+	import CartPanel from './CartPanel.svelte'
+	import { cartPanelOpen, product } from '$lib/store/productStore'
 
-  $: if ($product.length !== 0) {
-    console.log("product", $product);
-    cartPanelOpen.set(true);
-  }
+	$: if ($product.length !== 0) {
+		console.log('product', $product)
+		cartPanelOpen.set(true)
+	}
 </script>
 
-<!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-  on:click={() => cartPanelOpen.set(true)}
-  class="rounded-full bg-primary-dark bg-opacity-30 hover:bg-opacity-80 duration-150 p-2 cursor-pointer fixed top-2 left-2">
-  {#if $product.length > 0}
-    <div
-      class="h-4 w-4 animate-pulse rounded-full absolute -top-1 -right-1 text-[12px] bg-pink-600 font-body ring-2 ring-primary text-white flex justify-center items-center z-[1000]">
-      {$product.length}
-    </div>
-  {/if}
+	on:click={() => cartPanelOpen.set(true)}
+	class="rounded-full bg-primary-dark bg-opacity-30 hover:bg-opacity-80 duration-150 p-2 cursor-pointer fixed top-2 left-2">
+	{#if $product.length > 0}
+		<div
+			class="h-4 w-4 animate-pulse rounded-full absolute -top-1 -right-1 text-[16px] bg-pink-600 font-body ring-2 ring-primary text-white flex justify-center items-center z-[1000]">
+			{$product.length}
+		</div>
+	{/if}
 
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    class="w-5 h-5 stroke-white"
-    class:inactive={$product.length == 0}>
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-  </svg>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		class="w-8 h-8 stroke-white"
+		class:inactive={$product.length == 0}>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+	</svg>
 </div>
 
 <CartPanel showCart={$cartPanelOpen} />
 
 <style>
-  .inactive {
-    opacity: 0.6;
-  }
+	.inactive {
+		opacity: 0.6;
+	}
 </style>
--- app/src/routes/(store)/+page.svelte
+++ app/src/routes/(store)/+page.svelte
@@ -1,65 +1,40 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import type { Drink } from "$lib/types.js";
+	import { onMount } from 'svelte'
+	import type { Drink } from '$lib/types.js'
+	import { pb } from '$lib/pocketbase'
+	import Carousel from '$lib/components/Carousel.svelte'
+	import DrinkCard from '$lib/components/DrinkCard.svelte'
 
-  import { pb } from "$lib/pocketbase";
-  import Carousel from "$lib/components/Carousel.svelte";
-  import DrinkCard from "$lib/components/DrinkCard.svelte";
-  import { cartPanelOpen, product } from "$lib/store/productStore.js";
-  import { browser } from "$app/environment";
-  import toast from "svelte-french-toast";
-  export let data;
+	export let data
 
-  let drinks: Drink[] = data.drinks;
+	let drinks: Drink[] = data.drinks
 
-  let touchTimeout: any;
-
-  // wait 2 minutes before clearing the product
-  const clearTouchTimeout = () => {
-    // clearTimeout(touchTimeout);
-    // touchTimeout = setTimeout(() => {
-    //   if ($product.length === 0) return;
-    //   console.log("first");
-    //   product.clear();
-    //   cartPanelOpen.set(false);
-    //   toast.success("Der Warenkorb wurde aufgrund von Inaktivität geleert.");
-    // }, 60000 * 2);
-  };
-
-  onMount(() => {
-    if (browser) {
-      window.addEventListener("mousemove", clearTouchTimeout);
-      window.addEventListener("touchstart", clearTouchTimeout);
-      window.addEventListener("touchend", clearTouchTimeout);
-    }
-
-    pb.collection("drinks").subscribe<Drink>("*", ({ action, record }) => {
-      switch (action) {
-        case "create":
-          record = { ...record, cover: pb.getFileUrl(record, record.cover) };
-          drinks = [record, ...drinks];
-          break;
-        case "update":
-          let i = drinks.findIndex((drink) => drink.id === record.id);
-          drinks[i] = { ...record, cover: pb.getFileUrl(record, record.cover) };
-          break;
-        case "delete":
-          drinks = drinks.filter((drink) => drink.id !== record.id);
-          break;
-        default:
-          break;
-      }
-    });
-    return () => (
-      pb.collection("drinks").unsubscribe(), clearTimeout(touchTimeout)
-    );
-  });
+	onMount(() => {
+		pb.collection('drinks').subscribe<Drink>('*', ({ action, record }) => {
+			switch (action) {
+				case 'create':
+					record = { ...record, cover: pb.getFileUrl(record, record.cover) }
+					drinks = [record, ...drinks]
+					break
+				case 'update':
+					let i = drinks.findIndex(drink => drink.id === record.id)
+					drinks[i] = { ...record, cover: pb.getFileUrl(record, record.cover) }
+					break
+				case 'delete':
+					drinks = drinks.filter(drink => drink.id !== record.id)
+					break
+				default:
+					break
+			}
+		})
+		return () => pb.collection('drinks').unsubscribe()
+	})
 </script>
 
 <ul class="product_list">
-  <Carousel perPage={1} autoplay={5000} dots={false}>
-    {#each drinks.filter((d) => d.active) as drink (drink.id)}
-      <DrinkCard {drink} />
-    {/each}
-  </Carousel>
+	<Carousel perPage={1} autoplay={50000} dots={false}>
+		{#each drinks.filter(d => d.active) as drink (drink.id)}
+			<DrinkCard {drink} />
+		{/each}
+	</Carousel>
 </ul>
